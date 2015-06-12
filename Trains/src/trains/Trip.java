9c722b0f6cdb8fc2ff02370e9903e5789c6c5e19
madjonicar <jmpradillak@gmail.com>
2015-06-11 20:49:09 -0500

    Automated setup

--- Trains/src/trains/Trip.java
+++ Trains/src/trains/Trip.java
@@ -1,5 +1,6 @@
 package trains;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,6 @@ import java.util.List;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author jmkanayetp
@@ -18,73 +18,54 @@ public class Trip {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {        
-        // People to travel        
-        Person person1 = new Person();
-        Person person2 = new Person();
-        Person person3 = new Person();
-        Person person4 = new Person();
-        Person person5 = new Person();
-        Person person6 = new Person();
-        //Initialize values
-        person1.setId(1);
-        person2.setId(2);
-        person3.setId(3);
-        person4.setId(4);
-        person5.setId(5);
-        person6.setId(6);        
-        person1.setFullName("Full name1");
-        person2.setFullName("Full name2");
-        person3.setFullName("Full name3");
-        person4.setFullName("Full name4");
-        person5.setFullName("Full name5");
-        person6.setFullName("Full name6");        
-        //Create first person's list into the first family        
-        List<Person> membersFamily1 = new ArrayList<>();
-        //Add persons
-        membersFamily1.add(person1);
-        membersFamily1.add(person2);
-        membersFamily1.add(person3);
-        membersFamily1.add(person4);
-        membersFamily1.add(person5);
-        membersFamily1.add(person6);
-        //Set persons (members)
-        Family family1 = new Family();     
-        family1.setIdFamily(1);
-        family1.setPersons(membersFamily1);        
-        //Create Wagons        
-        Wagon wagon1 = new Wagon();
-        Wagon wagon2 = new Wagon();
-        Wagon wagon3 = new Wagon();
-        //Set attributes
-        wagon1.setIdWagon(1);
-        wagon1.setTotalChairsWagon(20);
-        wagon1.setOccupiedChairsWagon(0);
-        
-        wagon2.setIdWagon(2);
-        wagon2.setTotalChairsWagon(20);
-        wagon2.setOccupiedChairsWagon(0);
-        
-        wagon3.setIdWagon(3);
-        wagon3.setTotalChairsWagon(20);
-        wagon3.setOccupiedChairsWagon(0);
-        
+    public static void main(String[] args) {
+        //Create List of families
+        List<List<Person>> familiesList = new ArrayList<>();
+        //Create families
+        for (int i = 0; i < 5; i++) {
+            List<Person> membersFamily = new ArrayList<>();
+            familiesList.add(membersFamily);
+        }
+        //Create persons
+        for (int i = 0; i < 100; i++) {
+            Person person = new Person();
+            person.setId(i);
+            person.setFullName("Full name" + i);
+            familiesList.get(i % 5).add(person);
+        }
+        //Create List of families
+        List<Family> families = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            //Set persons (members)
+            Family family = new Family();
+            family.setIdFamily(i);
+            family.setPersons(familiesList.get(i));
+            families.add(family);
+        }
+        //List of wagons
+        List<Wagon> wagons = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Wagon wagon = new Wagon();
+            wagon.setIdWagon(i);
+            wagon.setTotalChairsWagon(i*10);
+            wagon.setOccupiedChairsWagon(0);
+            wagons.add(wagon);
+        }
+                 
         //Create the train        
-        List<Wagon> wagons = new ArrayList<>();      
-        Train train = new Train();    
-        train.setWagons(wagons);        
-        // Reservation        
-        Reservation reservation = new Reservation();        
-        reservation.setIdReservation(1);
-        reservation.setCostReservation(300000);
-        reservation.setDateReservation("25-06-2015");
-        reservation.setFamily(family1);
-        
-        System.out.println(family1.getPersons().get(0).getFullName()
-                           + " is a member of the family " + family1.getIdFamily()
-                           + " with the reservation " + reservation.getIdReservation()
-                           + " and date " + reservation.getDateReservation());
+        Train train = new Train();
+        train.setWagons(wagons);
+        //List of reservations
+        List<Reservation> reservations = new ArrayList<>();
+        // Reservations     
+        for (int i = 0; i < 5; i++) {
+            Reservation reservation = new Reservation();
+            reservation.setIdReservation(i);
+            reservation.setCostReservation(300000);
+            reservation.setDateReservation("25-06-2015");
+            reservation.setFamily(families.get(i));
+            reservations.add(reservation);
+        }
         
     }
-    
 }
